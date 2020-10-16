f68b8921c5885b7ac7f1f322819b6c66763885b3
Administrator <Administrator@1005-22>
2020-10-16 18:11:01 +0900

    10/16

---
+++ springreview2/src/main/java/com/docmall/service/MemberService.java
@@ -0,0 +1,39 @@
+package com.docmall.service;
+
+import com.docmall.dto.MemberDTO;
+
+import java.util.Date;
+
+import com.docmall.domain.MemberVO;
+
+public interface MemberService {
+	
+	// MemberVO 가져오기
+		public MemberVO readUserInfo(String mem_id) throws Exception;
+		
+		// 로그인
+		public MemberDTO login(MemberDTO dto) throws Exception; 
+		
+		// 회원가입
+		public void join(MemberVO vo) throws Exception;
+
+		// 아이디 중복체크
+		public int checkIdDuplicate(String mem_id) throws Exception;
+		
+		// 회원정보 수정
+		public void modifyUserInfo(MemberVO vo) throws Exception;
+		
+		// 비밀번호 변경
+		public void changePw(MemberDTO dto) throws Exception;
+		
+		// 회원 탈퇴
+		public void deleteUser(String mem_id) throws Exception;
+		
+		// 로그인 정보 쿠키 저장
+		public void saveCookie(String sessionKey, Date sessionLimit, String mem_id) throws Exception;
+		
+		// 쿠키에 저장된 세션값으로 로그인 정보 가져옴
+		public MemberVO checkUserSession(String value) throws Exception;
+		
+	
+}
